b285e516ed271ca641a64fb2399081091a8b791e
blacchoy@gmail.com <blacchoy@gmail.com>
2020-06-09 11:08:19 -0700

    add working js for number inputs

--- count-by/js/scripts.js
+++ count-by/js/scripts.js
@@ -1,25 +1,16 @@
-function triangle (side1,side2,side3) {
-  let result;
-if (side1 === side2 && side2 === side3) {
- result = "equilateral";
-} else if (side1 === side2 || side2 === side3 || side3 === side1) {
- result = "isosceles";
-} else if (side1 !== side2 && side2 !== side3 && side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
-  result = "scalene";
-} else {
-  result = "not triangle";
-  }
-  return result;
-}
-
 $(document).ready(function() {
-  $("form#triangle").submit(function(event) {
+  $("form#count").submit(function(event) {
     event.preventDefault();
-    const side1 = parseInt($("input#side1").val());
-    const side2 = parseInt($("input#side2").val());
-    const side3 = parseInt($("input#side3").val());
-    let answer = triangle(side1,side2,side3);
-    $(".output").text(answer);
-    $(".output").show();
+    const countTo = parseInt($("input#countTo").val());
+    const countBy = parseInt($("input#countBy").val());
+
+    for (i = 1; i <= countTo; i += countBy) {
+      if (Number.isInteger(countTo) && Number.isInteger(countBy)) {
+        $("#output").append("<li>" + i + "</li>");
+      } else {
+        alert("Enter a number!");
+      }
+    }
+
   });
-  });
+});
