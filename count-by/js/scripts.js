3db62f08157d313ab674627f64edf952bbe62dc0
blacchoy@gmail.com <blacchoy@gmail.com>
2020-06-09 11:56:28 -0700

    add working js logic for number inputs and other conditions
    Co-authored-by: JohnNils Olson <johnnils@gmail.com>

--- count-by/js/scripts.js
+++ count-by/js/scripts.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
   $("form#count").submit(function(event) {
+    $("li").remove();
     event.preventDefault();
+
     const countTo = parseInt($("input#countTo").val());
     const countBy = parseInt($("input#countBy").val());
 
-    for (i = 1; i <= countTo; i += countBy) {
-      if (Number.isInteger(countTo) && Number.isInteger(countBy)) {
-        $("#output").append("<li>" + i + "</li>");
-      } else {
-        alert("Enter a number!");
+    if (Number.isInteger(countTo) === false || Number.isInteger(countBy) === false) {
+      alert("Please enter a number!");
+    } else if (Math.sign(countTo) === -1 || Math.sign(countBy) === -1)  {
+      alert("Please enter a positive number!");
+    } else if (countBy > countTo) {
+      alert("Please enter a larger number above!");
+    } else {
+        for (i = 0; i <= countTo; i += countBy) {
+          $("#output").append("<li>" + i + "</li>")
       }
     }
-
   });
 });
